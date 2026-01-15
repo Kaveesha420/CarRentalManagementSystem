82757f20d3dd48ab70fa3ce62a0ad3e00cc94015
Kaveesha420 <icetkaveesha@gmail.com>
2026-01-16 00:05:13 +0530

    Created User Entity class

---
+++ src/main/java/ecom/icet/Model/Entity/User.java
@@ -0,0 +1,25 @@
+package ecom.icet.Model.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String role;
+}
