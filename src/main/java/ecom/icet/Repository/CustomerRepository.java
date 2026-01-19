fc5e8bca19d8afbdf1ed4124828f4ba10d10db35
Kaveesha420 <icetkaveesha@gmail.com>
2026-01-19 11:30:52 +0530

    Created CustomerRepository Repository Interface

---
+++ src/main/java/ecom/icet/Repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package ecom.icet.Repository;
+
+import ecom.icet.Model.Entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    Optional<Customer> findByEmail(String email);
+}
+
