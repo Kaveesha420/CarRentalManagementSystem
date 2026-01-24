5a2f218fd8598419a93b82a2b7b2e0a4f272fafd
Kaveesha420 <icetkaveesha@gmail.com>
2026-01-24 12:07:09 +0530

    created PaymentService Service Interface

--- src/main/java/ecom/icet/Service/PaymentService.java
+++ src/main/java/ecom/icet/Service/PaymentService.java
@@ -1,4 +1,11 @@
 package ecom.icet.Service;
 
+import ecom.icet.Model.Dto.PaymentDto;
+
+import java.util.List;
+
 public interface PaymentService {
+    PaymentDto addPayment(PaymentDto paymentDto);
+    List<PaymentDto> getAllPayments();
+    PaymentDto getPaymentByBookingId(Long bookingId);
 }
