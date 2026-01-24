2f6399def4ae95f869895070dce065dad6c0b945
Kaveesha420 <icetkaveesha@gmail.com>
2026-01-24 11:29:30 +0530

    created BookingService Service Interface

--- src/main/java/ecom/icet/Service/BookingService.java
+++ src/main/java/ecom/icet/Service/BookingService.java
@@ -1,4 +1,12 @@
 package ecom.icet.Service;
 
+import ecom.icet.Model.Dto.BookingDto;
+
+import java.util.List;
+
 public interface BookingService {
+    BookingDto addBooking(BookingDto bookingDto);
+    List<BookingDto> getAllBookings();
+    List<BookingDto> getBookingsByCustomerId(Long customerId);
+    BookingDto updateBookingStatus(Long id,String status);
 }
