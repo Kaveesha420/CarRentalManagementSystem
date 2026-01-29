273743b40c02b23ba93004f2fc72a5608d0648c4
Kaveesha420 <icetkaveesha@gmail.com>
2026-01-29 21:41:33 +0530

    Changed UserDto class for validation Rules

--- src/main/java/ecom/icet/Model/Dto/UserDto.java
+++ src/main/java/ecom/icet/Model/Dto/UserDto.java
@@ -1,5 +1,7 @@
 package ecom.icet.Model.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -9,8 +11,13 @@ import lombok.*;
 @ToString
 public class UserDto {
         private Long id;
+        @NotBlank(message = "Username is required")
         private String username;
+        @NotBlank(message = "Password is required")
         private String password;
+        @Email(message = "Invalid email format")
+        @NotBlank(message = "Email is required")
         private String email;
+        @NotBlank(message = "Role is required")
         private String role;
 }
