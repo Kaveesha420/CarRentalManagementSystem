58022f8b0a12ad699ac3a036f0deefcdf5a7163b
Kaveesha420 <icetkaveesha@gmail.com>
2026-01-29 21:47:03 +0530

    Changed CustomerDto class for validation Rules

--- src/main/java/ecom/icet/Model/Dto/CustomerDto.java
+++ src/main/java/ecom/icet/Model/Dto/CustomerDto.java
@@ -1,5 +1,8 @@
 package ecom.icet.Model.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -9,11 +12,23 @@ import lombok.*;
 @ToString
 public class CustomerDto {
         private Long id;
+
+        @NotBlank(message = "Name is required")
         private String name;
+
+        @NotBlank(message = "Address is required")
         private String address;
+
+        @Email(message = "Invalid email format")
+        @NotBlank(message = "Email is required")
         private String email;
+
+        @Pattern(regexp = "^\\d{10}$", message = "Contact number must be exactly 10 digits")
         private String contactNumber;
+
+        @NotBlank(message = "Nic is required")
         private String nic;
+
         private UserDto user;
 }
 
