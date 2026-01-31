d866b08a0313c88f1505b8ca5143d5964abfa097
Kaveesha420 <icetkaveesha@gmail.com>
2026-01-31 13:52:08 +0530

    Changed Driver Entity for Soft delete.

--- src/main/java/ecom/icet/Model/Entity/Driver.java
+++ src/main/java/ecom/icet/Model/Entity/Driver.java
@@ -2,6 +2,8 @@ package ecom.icet.Model.Entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Getter
 @Setter
@@ -9,6 +11,8 @@ import lombok.*;
 @NoArgsConstructor
 @ToString
 @Entity
+@SQLDelete(sql = "UPDATE drivers SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted = false")
 public class Driver {
 
     @Id
@@ -18,7 +22,9 @@ public class Driver {
     private String licenseNo;
     @Column(nullable = false)
     private String contactNo;
-
     @Column(nullable = false)
     private String status;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
