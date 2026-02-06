43deafdb9496ec2f29658cf86fe9117741d1f6b3
Kaveesha420 <icetkaveesha@gmail.com>
2026-02-06 13:39:21 +0530

    Created JwtUtil class for Genarate Token and check validation

---
+++ src/main/java/ecom/icet/Util/JwtUtil.java
@@ -0,0 +1,24 @@
+package ecom.icet.Util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final int expirationTime = 1000 * 60 * 60 * 10;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(key)
+                .compact();
+    }
+}
